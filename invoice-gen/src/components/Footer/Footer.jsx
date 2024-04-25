c5e2192415d1b40f652161c0d729f35d5846d105
tusharrajput001 <devoptushar@gmail.com>
2024-04-25 18:00:43 +0530

    account part

--- invoice-gen/src/components/Footer/Footer.jsx
+++ invoice-gen/src/components/Footer/Footer.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import "./Footer.css"
-
+import React from 'react';
+import './Footer.css';
 
 function Footer() {
   return (
     <div className='footer-container'>
-        <div className='accountDetails'></div>
-        <div className='stamp'></div>
+      <div className='accountDetails'>
+        <div className='account-section'>
+          <div className='label'>Account Holder Name:</div>
+          <div className='value'>ATRENTERPRISE</div>
+        </div>
+        <div className='account-section'>
+          <div className='label'>Bank Account Number:</div>
+          <div className='value'>347405000892</div>
+        </div>
+        <div className='account-section'>
+          <div className='label'>Bank IFSC Code:</div>
+          <div className='value'>ICIC0003474</div>
+        </div>
+        <div className='account-section'>
+          <div className='label'>Bank Name:</div>
+          <div className='value'>ICICI BANK</div>
+        </div>
+        <div className='account-section'>
+          <div className='label'>Bank Branch Name:</div>
+          <div className='value'>Mahipalpur NEW DELHI</div>
+        </div>
+      </div>
+      <div className='stamp'></div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
