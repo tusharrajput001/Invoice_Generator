e0a48e4e6a75e9838ceeb0ff616fdcd4ca4bf645
tusharrajput001 <devoptushar@gmail.com>
2024-04-26 12:59:44 +0530

    ui completed

--- invoice-gen/src/components/Footer/Footer.jsx
+++ invoice-gen/src/components/Footer/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Footer.css';
+import stamp from "../../assets/STAMP.png"
 
 function Footer() {
   return (
     <div className='footer-container'>
+
       <div className='accountDetails'>
         <div className='account-section'>
           <div className='label'>Account Holder Name:</div>
-          <div className='value'>ATRENTERPRISE</div>
+          <div className='value'>ATR ENTERPRISES</div>
         </div>
         <div className='account-section'>
           <div className='label'>Bank Account Number:</div>
@@ -25,8 +27,24 @@ function Footer() {
           <div className='label'>Bank Branch Name:</div>
           <div className='value'>Mahipalpur NEW DELHI</div>
         </div>
+        <div className='terms'>
+          <h3>Terms and Conditions</h3>
+          <ol>
+            <li>This is electronic generated quotation.</li>
+            <li>All disputes are subject to NEW DELHI jurisdiction. </li>
+            <li>100% Payment against PI. </li>
+          </ol>
+
+        </div>
+
       </div>
-      <div className='stamp'></div>
+      
+      <div className='stamp'>
+        <p>Certified that the particular given above are true and correct</p>
+        <h2>For, ATR ENTERPRISES</h2>
+        <img src={stamp} alt="stamp"></img>
+        <p>Authorized Signatory</p>
+      </div>
     </div>
   );
 }
