41111a8b76407de0cd6632aeb9d0b1acef57c73f
tusharrajput001 <devoptushar@gmail.com>
2024-05-02 18:56:00 +0530

    fonts

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,23 +9,23 @@ function Footer() {
       <div className='accountDetails'>
         <div className='account-section'>
           <div className='label'>Account Holder Name:</div>
-          <div className='value'>ATR ENTERPRISES</div>
+          <div className='value'><input placeholder='ABC ENTERPISES'></input></div>
         </div>
         <div className='account-section'>
           <div className='label'>Bank Account Number:</div>
-          <div className='value'>347405000892</div>
+          <div className='value'><input placeholder='24732504842'></input></div>
         </div>
         <div className='account-section'>
           <div className='label'>Bank IFSC Code:</div>
-          <div className='value'>ICIC0003474</div>
+          <div className='value'><input placeholder='ICIC0233477'></input></div>
         </div>
         <div className='account-section'>
           <div className='label'>Bank Name:</div>
-          <div className='value'>ICICI BANK</div>
+          <div className='value'><input placeholder='ICICI BANK'></input></div>
         </div>
         <div className='account-section'>
           <div className='label'>Bank Branch Name:</div>
-          <div className='value'>Mahipalpur NEW DELHI</div>
+          <div className='value'><input placeholder='Mayapuri NEW DELHI'></input></div>
         </div>
         <div className='terms'>
           <h3>Terms and Conditions</h3>
@@ -41,8 +41,7 @@ function Footer() {
       
       <div className='stamp'>
         <p>Certified that the particular given above are true and correct</p>
-        <h2>For, ATR ENTERPRISES</h2>
-        <img src={stamp} alt="stamp"></img>
+        <h2><input placeholder='For, ABC ENTERPRISES'></input></h2>
         <p>Authorized Signatory</p>
       </div>
     </div>
