43fb73c8c4bb84b23a34599003571a5af87d5ef2
tusharrajput001 <devoptushar@gmail.com>
2024-05-02 19:19:21 +0530

    HEading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 
-import { ReactToPrint } from 'react-to-print';
-import PrintableContent from './components/PrintableContent';
+import { ReactToPrint } from "react-to-print";
+import PrintableContent from "./components/PrintableContent";
 
 function App() {
   const componentRef = useRef();
 
   return (
     <>
+      <h1 className="Heading">PROFORMA INVOICE GENERATOR</h1>
       <ReactToPrint
-        trigger={() => <button className='printBtn'>Print</button>}
+        trigger={() => <button className="printBtn">Print</button>}
         content={() => componentRef.current}
-        documentTitle='Printable Content'
+        documentTitle="Printable Content"
       />
+
       <div className="container">
         <PrintableContent ref={componentRef} />
       </div>
