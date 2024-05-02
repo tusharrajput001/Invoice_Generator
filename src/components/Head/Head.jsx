844c3893d255532d65d8c82eeb7eea9c44964946
tusharrajput001 <devoptushar@gmail.com>
2024-05-02 18:47:35 +0530

    Head changes

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -4,13 +4,13 @@ import "./Head.css"
 function Head() {
   return (
     <div className="head-part">
-      <h2>ATR ENTERPRISES</h2>
-      <p>Rz-82/345 Shivpuri</p>
-      <p>West Sagarpur</p>
-      <p>NEW DELHI - 110046</p>
-      <p>9711484980</p>
-      <a href="enterprisesatr45@gmail.com">enterprisesatr45@gmail.com</a>
-      <p>GSTIN: 07BQLPR2638P1Z0</p>
+      <h2><input className="head-inp" placeholder="ABC ENTERPRISES"></input></h2>
+      <p><input className="head-inp" placeholder="E-21/22 PHASE 1 "></input></p>
+      <p><input className="head-inp" placeholder="West Janakpuri"></input></p>
+      <p><input className="head-inp" placeholder="NEW DELHI - 110054"></input></p>
+      <p><input className="head-inp" placeholder="95655XXXXX"></input></p>
+      <a><input className="head-inp" placeholder="abc@gmail.com"></input></a>
+      <p><input className="head-inp gst-inp" placeholder="GSTIN: 65AG46E2W4FG1DA"></input></p>
     </div>
   );
 }
